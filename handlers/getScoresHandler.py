bb22e2d5c3972655a6cf11baca6d0a4a8e3918b1
Howl <the@howl.moe>
2016-04-26 14:16:20 +0200

    .FIX. Hopefully fix getscores

--- handlers/getScoresHandler.py
+++ handlers/getScoresHandler.py
@@ -44,7 +44,7 @@ class handler(tornado.web.RequestHandler):
 
 			# Data to return
 			data = ""
-			data += bmap.getData()
+			data += bmap.getData(username)
 			data += lboard.getScoresData()
 			self.write(data)
 		except exceptions.invalidArgumentsException:
