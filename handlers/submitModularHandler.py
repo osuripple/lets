76bea80dd45b9b74ca7c397d25274cfb1d1e66c9
Nyo <to@nyo.zz.mu>
2016-08-12 12:56:08 +0200

    .SCORES. .FIX. Don't send first place message if user is restricted

--- handlers/submitModularHandler.py
+++ handlers/submitModularHandler.py
@@ -281,7 +281,7 @@ class handler(SentryMixin, requestHelper.asyncRequestHandler):
 
 
 				# send message to #announce if we're rank #1
-				if newScoreboard.personalBestRank == 1 and s.completed == 3:
+				if newScoreboard.personalBestRank == 1 and s.completed == 3 and restricted == False:
 					annmsg = "[https://ripple.moe/?u={} {}] achieved rank #1 on [https://osu.ppy.sh/b/{} {}] ({})".format(userID, username, beatmapInfo.beatmapID, beatmapInfo.songName, gameModes.getGamemodeFull(s.gameMode))
 					params = urlencode({"k": glob.conf.config["server"]["apikey"], "to": "#announce", "msg": annmsg})
 					requests.get("{}/api/v1/fokabotMessage?{}".format(glob.conf.config["server"]["banchourl"], params))
