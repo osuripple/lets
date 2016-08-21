cbe12f9aaaca968806514c08f42942a52d5210c8
Nyo <to@nyo.zz.mu>
2016-08-21 14:09:25 +0200

    Better arguments handling in check-updates

--- handlers/checkUpdatesHandler.py
+++ handlers/checkUpdatesHandler.py
@@ -3,23 +3,21 @@ from helpers import logHelper as log
 import tornado.web
 import tornado.gen
 import requests
+from urllib.parse import urlencode
 
 class handler(requestHelper.asyncRequestHandler):
 	@tornado.web.asynchronous
 	@tornado.gen.engine
 	def asyncGet(self):
 		try:
-			args = "?"
-			for i in self.request.arguments:
-				args += "&"+i+"="+self.get_argument(i)
-			#if requestHelper.checkArguments(self.request.arguments, ["stream", "action"]) == False:
-			#	self.write("missing params")
-			#	return
-
-			url = "https://osu.ppy.sh/web/check-updates.php{}".format(args)
-			print(str(url))
-			response = requests.get(url)
-			print(str(response.text))
+			args = {}
+			if "stream" in self.request.arguments:
+				args["stream"] = self.get_argument("stream")
+			if "action" in self.request.arguments:
+				args["action"] = self.get_argument("action")
+			if "time" in self.request.arguments:
+				args["time"] = self.get_argument("time")
+			response = requests.get("https://osu.ppy.sh/web/check-updates.php?{}".format(urlencode(args)))
 			self.write(response.text)
 		except Exception as e:
 			log.error("check-updates failed: {}".format(e))
