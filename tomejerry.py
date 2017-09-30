b7b765d8d4c67ba7390643eb0c4a9edfc0df94d4
Giuseppe Guerra <to@nyo.zz.mu>
2017-09-30 23:55:15 +0200

    tomejerry: don't force completed=3 when running with -i argument

--- tomejerry.py
+++ tomejerry.py
@@ -255,7 +255,7 @@ if __name__ == "__main__":
 	elif args.id is not None:
 		# Score ID recalc
 		print("> Recalculating pp for score ID {}".format(args.id))
-		scores = glob.db.fetchAll("SELECT * FROM scores LEFT JOIN beatmaps ON scores.beatmap_md5 = beatmaps.beatmap_md5 WHERE scores.play_mode != 2 AND scores.completed = 3 AND scores.id = %s;", [args.id])
+		scores = glob.db.fetchAll("SELECT * FROM scores LEFT JOIN beatmaps ON scores.beatmap_md5 = beatmaps.beatmap_md5 WHERE scores.play_mode != 2 AND scores.id = %s;", [args.id])
 		massRecalc(scores, workers)
 	elif args.gamemode is not None:
 		# game mode recalc
