a92c1b4f76435f2cfadd73ec4394f6eb5bbc2408
Giuseppe Guerra <to@nyo.zz.mu>
2019-08-19 10:52:19 +0200

    Ensure replays backup folder exists

--- lets.py
+++ lets.py
@@ -137,7 +137,8 @@ if __name__ == "__main__":
 			".data/catch_the_pp",
 			glob.conf["BEATMAPS_FOLDER"],
 			glob.conf["SCREENSHOTS_FOLDER"],
-			glob.conf["FAILED_REPLAYS_FOLDER"]
+			glob.conf["FAILED_REPLAYS_FOLDER"],
+			glob.conf["REPLAYS_FOLDER"]
 		)
 		for i in paths:
 			if not os.path.exists(i):
