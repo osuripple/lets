604a31d693b2125c6f8d004d4865754c3fef0958
Giuseppe Guerra <to@nyo.zz.mu>
2020-02-24 17:39:37 +0100

    Temporarily turn off auto pp restriction for relax

--- handlers/submitModularHandler.pyx
+++ handlers/submitModularHandler.pyx
@@ -175,7 +175,7 @@ class handler(requestsManager.asyncRequestHandler):
 				midPPCalcException = e
 
 			# Restrict obvious cheaters
-			if (s.pp >= 800 and s.gameMode == gameModes.STD) and not restricted:
+			if s.pp >= 800 and s.gameMode == gameModes.STD and not restricted and not s.isRelax:
 				userUtils.restrict(userID)
 				userUtils.appendNotes(userID, "Restricted due to too high pp gain ({}pp)".format(s.pp))
 				log.cm(
