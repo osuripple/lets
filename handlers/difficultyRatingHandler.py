7b3a2b99883b9fe616cf7c8611dc4251b07468a2
Jacob <61052022+Airiuwu@users.noreply.github.com>
2021-10-22 20:41:38 -0700

    Create difficultyRatingHandler.py

---
+++ handlers/difficultyRatingHandler.py
@@ -0,0 +1,19 @@
+import requests
+import tornado.gen
+import tornado.web
+
+from common.log import logUtils as log
+from common.web import requestsManager
+
+class handler(requestsManager.asyncRequestHandler):
+	@tornado.web.asynchronous
+	@tornado.gen.engine
+	def asyncPost(self):
+		try:
+			headers = {'Content-type': 'application/json'}
+      data = self.request.body
+			response = requests.post("https://osu.ppy.sh/difficulty-rating", data=data, headers=headers)
+			self.write(response.text)
+		except Exception as e:
+			log.error("difficulty-rating failed: {}".format(e))
+			self.write("")
